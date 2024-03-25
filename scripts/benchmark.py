842672ed6370a9269808657ed31e8ecec0d01446
Ethan Hann <ethanhann@gmail.com>
2024-03-24 20:07:25 -0400

    Cleanup comments and add iterations to specify range

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,39 +1,22 @@
-# import my_python_module.crusty as rust_lib
-import json
 import profile
-import pickle
 
 from crusty import set_string_item, get_string_item, set_binary_item, get_binary_item, get_string_item_decompressed, \
     set_string_item_compressed, get_binary_item_decompressed, set_binary_item_compressed, print_cache_size
 
 
 def main():
-    """
-    Need to evaluate 3 test cases:
-    1. set/get of PyString
-        Set 3ms / Get 10ms
-    2. set/get of python string / rust pickled binary data
-        Set 3ms / Get 9ms
-    3. set/get of python pickled binary data / rust Vec<u8>
-        Set 1.384 / Get 0.534
-
-    Conclusion (so far)... passing bytes through the function boundary is slow.
-
-    :return:
-    """
     print("Profiling...")
-    with open("../large-file.json", 'r') as file:
-        # Load the JSON data from the file
+    with open("./large-file.json", 'r') as file:
         big_str = file.read()
 
-    # pickled_big_str = pickle.dumps(big_str)
     big_bytes = bytes(big_str, "utf-8")
+    iterations = 10
 
     # This was... Set 3ms / Get 9ms
     print("Set/Get Big String")
     profiler = profile.Profile()
     profiler.runctx("""
-for _ in range(1):
+for _ in range(iterations):
     set_string_item("big_str", big_str)
     get_string_item("big_str")
     print_cache_size()
@@ -44,7 +27,7 @@ for _ in range(1):
     print("Set/Get Big Python Binary")
     profiler = profile.Profile()
     profiler.runctx("""
-for _ in range(1):
+for _ in range(iterations):
     set_binary_item("big_bytes", big_bytes)
     get_binary_item("big_bytes")
     print_cache_size()
@@ -55,7 +38,7 @@ for _ in range(1):
     print("Set/Get Compressed String")
     profiler = profile.Profile()
     profiler.runctx("""
-for _ in range(1):
+for _ in range(iterations):
     set_string_item_compressed("big_str", big_str)
     get_string_item_decompressed("big_str")
     print_cache_size()
@@ -66,7 +49,7 @@ for _ in range(1):
     print("Set/Get Compressed Python Binary")
     profiler = profile.Profile()
     profiler.runctx("""
-for _ in range(1):
+for _ in range(iterations):
     set_binary_item_compressed("big_bytes", big_bytes)
     get_binary_item_decompressed("big_bytes")
     print_cache_size()
